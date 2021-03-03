8708e32ffdb161d5686335f7f537eed664792986
P3bble <aoa987@gmail.com>
2021-03-03 00:55:35 -0600

    Got to page 57 in the text

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time 
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -36,15 +37,13 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
 
+        print("self.browser.page_source".upper() + self.browser.page_source) 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-        )
+        self.assertIn('Buy peacock feathers', [row.text for row in rows])
+
         # There is still a text box inviting here to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on here list
         # Edith wonders whether the site will remember her list. Then she sees
--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,18 @@
+from django.http.response import HttpResponseBadRequest
 from django.urls import resolve 
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+import re
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -14,5 +21,23 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        assert(response.content.decode(), expected_html)
+        self.assertEqual(self.remove_csrf(response.content.decode()), 
+                         self.remove_csrf(expected_html))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        print("EXPECTED_HTML:" + expected_html)
+        print("RESPONSE_CONTENT:" + self.remove_csrf(response.content.decode()))
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
