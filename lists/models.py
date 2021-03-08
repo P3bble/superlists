46a7760cd12e88be3e5c81fd6aa1ad10392abf66
P3bble <aoa987@gmail.com>
2021-03-07 22:12:47 -0600

    Updated the database to handle the new lists object page 101

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class List(models.Model):
+    pass 
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(default=None, to=List, on_delete=models.CASCADE)
